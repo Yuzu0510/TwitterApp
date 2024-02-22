1f66dd4b9628edb243728faacca54f3ccf8a7670
Koichi Wada <sakura.waffle.0510@gmail.com>
2024-02-23 02:11:16 +0900

    再度コミット

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var stringListData = ["dummyA", "dummyB", "dummyC", "dummyC"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+       
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    // リストの数を指定する　passlistの配列の個数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    // リストの中身を出力する
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "list", for: indexPath)
+        
+        cell.textLabel?.text = stringListData[indexPath.row]
+        return UITableViewCell()
+    }
+}
