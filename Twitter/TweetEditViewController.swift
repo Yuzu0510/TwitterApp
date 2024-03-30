42739ce375095481d985d3833f578cadfc114f2f
Koichi Wada <sakura.waffle.0510@gmail.com>
2024-03-30 13:36:07 +0900

    キャンセルボタン、ポストするボタンを追加した

--- Twitter/TweetEditViewController.swift
+++ Twitter/TweetEditViewController.swift
@@ -2,28 +2,34 @@
 //  TweetEditViewController.swift
 //  Twitter
 //
-//  Created by 和田康一 on 2024/03/30.
+//  Created by 和田康一 on 2024/03/24.
 //
 
 import UIKit
-
+/// ツイート編集画面
 class TweetEditViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var userIconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var postContentTextField: UITextField!
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // MARK: - IBActions
+    
+    ///　ポストボタン タップイベント
+    @IBAction func postButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    /// キャンセルボタン タップイベント
+    @IBAction func closeButtonTapped(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
 }
+
