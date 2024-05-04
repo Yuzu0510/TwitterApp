c38fd0b4b620db31ad49b19d428e1b498aa2e6f8
Koichi Wada <sakura.waffle.0510@gmail.com>
2024-05-04 16:52:27 +0900

    データモデルを別ファイルにしました。

---
+++ Twitter/TweetDataModel.swift
@@ -0,0 +1,22 @@
+//
+//  TweetDataModel.swift
+//  Twitter
+//
+//  Created by 和田康一 on 2024/05/04.
+//
+
+import UIKit
+import RealmSwift
+
+// データモデル
+class tweetDataModel: Object {
+    // 管理用 ID。プライマリーキー
+    @Persisted var id: String = UUID().uuidString //データを一意に識別するための識別子
+    // ツイート本文
+    @Persisted var tweetData: String = ""
+    // ユーザー名
+    @Persisted var userName: String = ""
+    // ユーザーアイコン
+    @Persisted var image: Data
+    
+}
--- Twitter/TweetEditViewController.swift
+++ Twitter/TweetEditViewController.swift
@@ -13,19 +13,6 @@ protocol TweetEditViewControllerDelegate: AnyObject {
     func timeLineIconUpdate()
 }
 
-// データモデル
-class tweetDataModel: Object {
-    // 管理用 ID。プライマリーキー
-    @Persisted var id: String = UUID().uuidString //データを一意に識別するための識別子
-    // ツイート本文
-    @Persisted var tweetData: String = ""
-    // ユーザー名
-    @Persisted var userName: String = ""
-    // ユーザーアイコン
-    @Persisted var image: Data
-    
-}
-
 /// ツイート編集画面
 class TweetEditViewController: UIViewController {
     
